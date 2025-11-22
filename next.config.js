27192c7f4a34be5f3d372b36b3bb1ca14429005d
sasha0012233 <cahaya2013@yandex.ru>
2025-11-22 20:32:22 +0300

    merge: resolve conflicts

--- next.config.js
+++ next.config.js
@@ -11,4 +11,9 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
+};
